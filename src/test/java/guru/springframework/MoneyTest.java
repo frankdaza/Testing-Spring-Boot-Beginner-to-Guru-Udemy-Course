8727b2926ba09151f03a4965db6f05397e764a65
Frank Edward Daza González <frank.daza2@gmail.com>
2019-05-28 20:48:52 -0500

    The Root of All Evil.

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -7,29 +7,25 @@ import org.junit.jupiter.api.Test;
 public class MoneyTest {
 
     @Test
-    void testMultiplicationDollar() {
-        Money five = Money.dollar(5);
-        Assertions.assertEquals(Money.dollar(10), five.times(2));
-        Assertions.assertEquals(Money.dollar(15), five.times(3));
+    void testMultiplication() {
+        Money fiveDollar = Money.dollar(5);
+        Assertions.assertEquals(Money.dollar(10), fiveDollar.times(2));
+        Assertions.assertEquals(Money.dollar(15), fiveDollar.times(3));
+
+        Money fiveFranc = Money.franc(5);
+        Assertions.assertEquals(Money.franc(10), fiveFranc.times(2));
+        Assertions.assertEquals(Money.franc(15), fiveFranc.times(3));
     }
 
     @Test
-    void testEqualityDollar() {
+    void testEquality() {
         Assertions.assertEquals(Money.dollar(5), Money.dollar(5));
         Assertions.assertNotEquals(Money.dollar(5), Money.dollar(8));
-    }
 
-    @Test
-    void testMultiplicationFranc() {
-        Money five = Money.franc(5);
-        Assertions.assertEquals(Money.franc(10), five.times(2));
-        Assertions.assertEquals(Money.franc(15), five.times(3));
-    }
-
-    @Test
-    void testEqualityFranc() {
         Assertions.assertEquals(Money.franc(5), Money.franc(5));
         Assertions.assertNotEquals(Money.franc(5), Money.franc(8));
+
+        Assertions.assertNotEquals(Money.dollar(5), Money.franc(5));
     }
 
     @Test
